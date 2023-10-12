98b3b6a79f3042e6f56057f92b794b2328a7c37c
Dabasi Csaba <mistercsabzon@gmail.com>
2023-10-12 22:13:54 +0200

    Ujra probál

--- src/main/java/Rabositas.java
+++ src/main/java/Rabositas.java
@@ -1,7 +1,13 @@
 public class Rabositas {
     public static void main(String[] args) {
         System.out.println("Csináljunk valamit");
-        boolean isprison = false;
+        boolean [] rab = new boolean [100];
+        System.out.println("Mindenki zárva lesz ,ezért false érték a kezdő");
+        for (int i = 0; i < 100; i++) {
+            rab[i]=false;
+
+        }
+
 
 
 
