7cd3ec0ad90370c09bc26928f6ea61b9fc2a6d79
Dabasi Csaba <mistercsabzon@gmail.com>
2023-10-13 21:24:59 +0200

    Ujra probál

--- src/main/java/Rabositas.java
+++ src/main/java/Rabositas.java
@@ -1,17 +1,32 @@
 public class Rabositas {
     public static void main(String[] args) {
         System.out.println("Csináljunk valamit");
-        boolean [] rab = new boolean [100];
+        boolean[] rab = new boolean[101];
         System.out.println("Mindenki zárva lesz ,ezért false érték a kezdő");
-        for (int i = 0; i < 100; i++) {
-            rab[i]=false;
+        for (int i = 0; i < 101; i++) {
+            rab[i] = false;
 
         }
-        for (int i = 0; i <100 ; i++) {
-            System.out.println(rab[i]);
+        for (int i = 0; i < 101; i++) {
+            for (int j = 0; j < 101; j++) {
+                if (i * j <= 100) {
+                    if (rab[i * j] == false) {
+                        rab[i * j] = true;
+                    } else if (rab[i * j] == true) {
+                        rab[i * j] = false;
+                    }
+                }
+
+            }
 
         }
 
-
+        for (int i = 1; i < 101; i++) {
+            System.out.print(rab[i]+" ");
+            if (rab[i] == true) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
     }
 }
